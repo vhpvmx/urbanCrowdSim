93eea52a6c952a3ff0570ecb6bc914637ca7d08f
jramirez79 <jeramfl@gmail.com>
2017-03-07 18:16:19 +0100

    Add files via upload

--- Header/cFrameClientHandler.h
+++ Header/cFrameClientHandler.h
@@ -31,20 +31,22 @@ private:
 	unsigned int ui_height;
 	unsigned int ui_BffrSz;
 	unsigned int ui_BffrSzBytes;
-
+	bool	b_cmbndSrcImg;
 
 public:
-	cFrameClientHandler(){
-		pc_frameBuffer = pc_depthBuffer = nullptr;
-		pf_frameBuffer = pf_depthBuffer = nullptr;
-		ui_width=ui_height=ui_BffrSz=ui_BffrSzBytes=0;
-	};
 
-	cFrameClientHandler(const unsigned int width,const unsigned int height){setFramesSizes(width,height);}
+	cFrameClientHandler(){
+		initData();
+		}
+
+	cFrameClientHandler(const unsigned int width,const unsigned int height){initData();setFramesSizes(width,height);}
+
 
 	~cFrameClientHandler(){
 		deleteData();
-	};
+	}
+
+
 
 	char * getFrameBuffer(){return pc_frameBuffer;}
 	char * getDepthBuffer(){return pc_depthBuffer;}
@@ -55,44 +57,73 @@ public:
 	unsigned int getWidth(){return ui_width;}
 	unsigned int getHeight(){return ui_height;}
 
-	bool processDataBuffer(const std::stringstream * val){
-		unsigned int tLenght = ui_BffrSzBytes+ui_BffrSzBytes+1;
-		unsigned int aux = val->str().length();
-		//char *data = new char[tLenght];
-
-		//std::cout<<" Successful data buffer sended with a size of : "<<aux<<" bytes"<<std::endl;
-		//std::cout<<" Estimated data size: "<<tLenght<<std::endl;
-		if(aux==0 || aux!=(tLenght))
-			return false;
-
-		//data = val->str().c_str();
-		//val->str().copy(data,tLenght,0);
-		val->str().copy(pc_frameBuffer,ui_BffrSzBytes,1);
-		val->str().copy(pc_depthBuffer,ui_BffrSzBytes,ui_BffrSzBytes+1);
-		std::cout<<" First byte of information: "
+	void printInfo(){
+		std::cout<<" UnNormalized data: "
 				<<(unsigned int)((unsigned char)pc_frameBuffer[263656])<<","
 				<<(unsigned int)((unsigned char)pc_frameBuffer[263657])<<","
 				<<(unsigned int)((unsigned char)pc_frameBuffer[263658])<<","
 				<<(unsigned int)((unsigned char)pc_frameBuffer[263659])<<","
 				<<(unsigned int)((unsigned char)pc_frameBuffer[263660])<<","<<std::endl;
 
-		std::cout<<"Congruence test, first byte of information is "
-				<<(unsigned int)((unsigned char)pc_depthBuffer[263656])<<","
-				<<(unsigned int)((unsigned char)pc_depthBuffer[263657])<<","
-				<<(unsigned int)((unsigned char)pc_depthBuffer[263658])<<","
-				<<(unsigned int)((unsigned char)pc_depthBuffer[263659])<<","
-				<<(unsigned int)((unsigned char)pc_depthBuffer[263660])<<","<<std::endl;
+				std::cout<<" Pointer: "<<pf_frameBuffer<<std::endl;
+				std::cout<<" Normalized data: "
+				<<pf_frameBuffer[263656]<<","
+				<<pf_frameBuffer[263657]<<","
+				<<pf_frameBuffer[263658]<<","
+				<<pf_frameBuffer[263659]<<","
+				<<pf_frameBuffer[263660]<<","<<std::endl;
 
+				std::cout<<" Normalized data depth buffer: "
+								<<pf_depthBuffer[0]<<std::endl;
+	}
+
+	bool processDataBuffer(std::stringstream * val){
+
+		unsigned int aux = val->str().length();
+		unsigned int tLenght = ui_BffrSzBytes + 1;
+
+		if(aux==0 ||( aux!=(tLenght) && aux!=(tLenght=(tLenght+ui_BffrSz))))
+			return false;
+
+		val->str().copy(pc_frameBuffer,ui_BffrSzBytes,1);
+
+		if(tLenght != ui_BffrSzBytes+1){
+			b_cmbndSrcImg = false;
+			val->str().copy(pc_depthBuffer,ui_BffrSz,ui_BffrSzBytes+1);
+		}
+
+
+
+		normalizeData();
 		return true;
 	}
 
 	void normalizeData(){
+		unsigned int x,conta=3;
 		if(pf_frameBuffer==nullptr||pf_depthBuffer==nullptr||pc_frameBuffer==nullptr||pc_depthBuffer==nullptr)
 			return;
-		for(unsigned int x=0;x<ui_BffrSzBytes;++x){
-			pf_frameBuffer[x]=(float)pc_frameBuffer[0]/255.0;
-			pf_depthBuffer[x]=(float)pc_depthBuffer[0]/255.0;
+
+		for(x=0;x<ui_BffrSzBytes;++x)
+			pf_frameBuffer[x]=(float)((unsigned int)((unsigned char)pc_frameBuffer[x]))/255.0;
+
+		if(!b_cmbndSrcImg){
+			for(x=0;x<ui_BffrSz;++x){
+				pf_depthBuffer[x]=(float)((unsigned int)((unsigned char)pc_depthBuffer[x]))/255.0;
+
+				/////////////////Combining both buffers in one /////////////////
+				pf_frameBuffer[conta]=pf_depthBuffer[x];
+				conta+=4;
+				/////////////////Combining both buffers in one /////////////////
+			}
 		}
+
+		//printInfo();
+	}
+
+	void initData(){
+		pc_frameBuffer = pc_depthBuffer = nullptr;
+		pf_frameBuffer = pf_depthBuffer = nullptr;
+		ui_width=ui_height=ui_BffrSz=ui_BffrSzBytes=0;
 	}
 
 	void setFramesSizes(const unsigned int width,const unsigned int height){
@@ -102,12 +133,14 @@ public:
 		ui_BffrSz = width*height;
 		ui_BffrSzBytes = ui_BffrSz*4;
 		pc_frameBuffer = new char[ui_BffrSzBytes];
-		pc_depthBuffer = new char [ui_BffrSzBytes];
+		pc_depthBuffer = new char [ui_BffrSz];
 		pf_frameBuffer = new float[ui_BffrSzBytes];
-		pf_depthBuffer = new float[ui_BffrSzBytes];
+		pf_depthBuffer = new float[ui_BffrSz];
+		b_cmbndSrcImg = true;
 	};
 
 	void deleteData(){
+		//std::cout<<"Destructor frmclinthndlr: "<<this<<std::endl;
 		try{
 			if(pc_frameBuffer!=nullptr)
 				delete pc_frameBuffer;
@@ -123,5 +156,5 @@ public:
 		ui_width=ui_height=ui_BffrSz=ui_BffrSzBytes=0;
 	}
 
-
+	void setCmbndSrcImg(bool flg){b_cmbndSrcImg = flg;}
 };
